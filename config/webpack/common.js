51b0785e4ba581934bacfda8a6ec16e3e6c458e7
David Copeland <davetron5000@gmail.com>
2018-05-07 11:14:27 -0400

    more bullshit

--- config/webpack/common.js
+++ config/webpack/common.js
@@ -1,5 +1,6 @@
 const HtmlPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   module: {
@@ -16,7 +17,14 @@ module.exports = {
   plugins: [
     new HtmlPlugin({
       template: "./src/html/index.html"
-    })
+    }),
+    new CopyWebpackPlugin(
+      [ 
+        { from: './src/images/**/*', to: './images', flatten: true }
+      ], 
+      {
+        debug: true
+      })
   ],
   entry: './src/js/index.js'
 };
